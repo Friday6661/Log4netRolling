cfd734bdd0a70c620f96e136eea3def6b723ee43
Friday6661 <jarifr26@gmail.com>
2023-07-25 21:53:59 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+public class Program
+{
+    private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+    static void Main()
+    {
+        // Konfigurasi log4net dari file eksternal
+        XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+        Logger.Info("Aplikasi telah dimulai.");
+        // Logging pesan dengan tingkatan yang berbeda
+        Logger.Debug("Ini adalah pesan tingkatan Debug");
+        Logger.Info("Ini adalah pesan tingkatan Info");
+        Logger.Warn("Ini adalah pesan tingkatan Warning");
+        Logger.Error("Ini adalah pesan tingkatan Error");
+        Logger.Fatal("Ini adalah pesan tingkatan Fatal");
+    }
+}
